52a516a571e4be88070b2b6c8fd078ca7533f51c
kushman20 <kushalmandal@gmail.com>
2021-12-06 20:21:09 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/libs/Testbase.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class Testbase {
+
+	
+	public void test()
+	{
+		System.out.println("this is test method");
+	}
+}
---
+++ src/test/java/com/qa/test/HomeTest.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+
+	@Test
+	public void homeTest()
+	{
+		System.out.println("this is home test page");
+	}
+	
+}
